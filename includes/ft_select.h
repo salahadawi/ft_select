dabdfc7f4e5001aed798e94b3f383128258a8765
sadawi <hahli144@hotmail.com>
2020-04-20 21:20:29 +0300

    add struct for args, text mode defines

--- includes/ft_select.h
+++ includes/ft_select.h
@@ -6,7 +6,7 @@
 /*   By: sadawi <sadawi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/10 12:20:24 by sadawi            #+#    #+#             */
-/*   Updated: 2020/04/20 16:09:41 by sadawi           ###   ########.fr       */
+/*   Updated: 2020/04/20 21:12:32 by sadawi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,24 @@
 # include <signal.h>
 # include <fcntl.h>
 
+# define INVERSE_VIDEO "\033[7m"
+# define UNDERLINE "\033[4m"
+# define NORMAL "\033[0m"
+
+typedef	struct		s_arg
+{
+	char			*str;
+	int				selected;
+	int				cursor;
+	struct s_arg	*prev;
+	struct s_arg	*next;
+}					t_arg;
+
 typedef struct		s_select
 {
 	struct termios	old;
 	struct termios	raw;
+	t_arg			*args;
 }					t_select;
 
 t_select			*g_select;
