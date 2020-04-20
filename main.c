6ac5da0e814389e8cbf4652732f2d70d19bef1f2
sadawi <hahli144@hotmail.com>
2020-04-20 16:10:26 +0300

    add signal detection, simple handling

--- main.c
+++ main.c
@@ -6,30 +6,32 @@
 /*   By: sadawi <sadawi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/18 16:15:00 by sadawi            #+#    #+#             */
-/*   Updated: 2020/04/18 22:33:18 by sadawi           ###   ########.fr       */
+/*   Updated: 2020/04/20 16:06:05 by sadawi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_select.h"
 
-void	handle_error(t_select *select, char *message)
+void	handle_error(char *message)
 {
 	ft_printf("Error: %s.\r\n", message);
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &select->old);
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &g_select->old);
 	exit(0);
 }
 
-void	terminal_raw_mode(t_select *select)
+void	set_terminal_raw_mode(void)
 {
-	struct termios raw;
+	if (tcgetattr(0, &g_select->raw) == -1)
+		handle_error("Invalid input/output descriptor");
+	g_select->raw.c_lflag &= ~(ECHO | ICANON);
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &g_select->raw) == -1)
+		handle_error("Invalid input/output descriptor");
+}
 
-	if (tcgetattr(0, &raw) == -1)
-		handle_error(select, "Invalid input/output descriptor");
-	raw.c_oflag &= ~(OPOST);
-	raw.c_iflag &= ~(IXON | ICRNL);
-	raw.c_lflag &= ~(ECHO | ICANON | ISIG | VSUSP);
-	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
-		handle_error(select, "Invalid input/output descriptor");
+void	restore_terminal_mode(void)
+{
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &g_select->old) == -1)
+		handle_error("Invalid input/output descriptor");
 }
 
 int		iscntrl(int c)
@@ -37,19 +39,62 @@ int		iscntrl(int c)
 	return (c < 32 || c == 127);
 }
 
+void	handle_signal_suspend(void)
+{
+	signal(SIGTSTP, SIG_DFL);
+	ioctl(0, TIOCSTI, "\032");
+}
+
+void	handle_signal_continue(void)
+{
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &g_select->old) == -1)
+		handle_error("Invalid input/output descriptor");
+	ft_printf("Raw terminal configuration restored!\n");
+}
+
+void	handle_signal_resize(void)
+{
+
+}
+
+void	handle_signal(int sig)
+{
+	ft_printf("Signal %d handled!\n", sig);
+	restore_terminal_mode();
+	ft_printf("Old terminal configuration restored!\n");
+	//free memory here
+	if (sig == SIGTSTP)
+		handle_signal_suspend();
+	else if (sig == SIGCONT)
+		handle_signal_continue();
+	else if (sig == SIGWINCH)
+		handle_signal_resize();
+	else
+		exit(0);
+}
+
+void	init_signal_handling(void)
+{
+	int i;
+
+	i = 0;
+	while (i <= SIGRTMAX)
+		signal(i++, handle_signal);
+}
+
 int		main(int argc, char **argv)
 {
-	t_select *select;
 	char c;
 
-	select = (t_select*)ft_memalloc(sizeof(t_select));
-	if (tcgetattr(0, &select->old) == -1)
-		handle_error(select, "Invalid input/output descriptor");
-	terminal_raw_mode(select);
+	init_signal_handling();
+	g_select = (t_select*)ft_memalloc(sizeof(t_select));
+	if (tcgetattr(0, &g_select->old) == -1)
+		handle_error("Invalid input/output descriptor");
+	set_terminal_raw_mode();
 	while (1)
 	{
 		if (read(0, &c, 1) == -1)
-			handle_error(select, "Read failed.");
+			handle_error("Read failed.");
 		if (c == 'q')
 			break;
 		if (iscntrl(c))
@@ -57,7 +102,7 @@ int		main(int argc, char **argv)
 		else
 			ft_printf("%c", c);
 	}
-	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &select->old) == -1)
-		handle_error(select, "Invalid input/output descriptor");
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &g_select->old) == -1)
+		handle_error("Invalid input/output descriptor");
 	return (0);
 }
