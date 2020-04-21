7b02c074136121a4be8a07b048a808763cfb3f2a
sadawi <hahli144@hotmail.com>
2020-04-21 14:59:55 +0300

    add termcap defines, some func prototypes

--- includes/ft_select.h
+++ includes/ft_select.h
@@ -6,7 +6,7 @@
 /*   By: sadawi <sadawi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/10 12:20:24 by sadawi            #+#    #+#             */
-/*   Updated: 2020/04/20 21:12:32 by sadawi           ###   ########.fr       */
+/*   Updated: 2020/04/21 14:58:47 by sadawi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,12 @@
 # include <signal.h>
 # include <fcntl.h>
 
-# define INVERSE_VIDEO "\033[7m"
-# define UNDERLINE "\033[4m"
-# define NORMAL "\033[0m"
+# define TEXT_INVERSE_VIDEO "mr"
+# define TEXT_UNDERLINE "us"
+# define TEXT_NORMAL "me"
+# define CURSOR_INVISIBLE "vi"
+# define CURSOR_VISIBLE "ve"
+# define CLEAR_SCREEN "cl"
 
 typedef	struct		s_arg
 {
@@ -43,5 +46,9 @@ typedef struct		s_select
 }					t_select;
 
 t_select			*g_select;
+
+void	ft_clear_screen(void);
+
+void	print_args(void);
 
 #endif
