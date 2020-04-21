b1a95af05a5ebc02c482be5d3dc7775fd17b9c83
sadawi <hahli144@hotmail.com>
2020-04-21 20:30:16 +0300

    add key support, refactor code

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: sadawi <sadawi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/18 16:15:00 by sadawi            #+#    #+#             */
-/*   Updated: 2020/04/21 14:59:00 by sadawi           ###   ########.fr       */
+/*   Updated: 2020/04/21 20:28:17 by sadawi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ void	restore_terminal_mode(void)
 {
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &g_select->old);
 	set_terminal(CURSOR_VISIBLE);
+	set_terminal(TEXT_NORMAL);
 }
 
 void	handle_error(char *message)
@@ -42,8 +43,11 @@ void	set_terminal_raw_mode(void)
 	if (tcgetattr(0, &g_select->raw) == -1)
 		handle_error("Invalid input/output descriptor");
 	g_select->raw.c_lflag &= ~(ECHO | ICANON);
+	g_select->raw.c_cc[VMIN] = 0;
+	g_select->raw.c_cc[VTIME] = 1;
 	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &g_select->raw) == -1)
 		handle_error("Invalid input/output descriptor");
+	set_terminal("ti");
 }
 
 int		iscntrl(int c)
@@ -98,11 +102,18 @@ void	init_signal_handling(void)
 
 void	print_args(void)
 {
-	t_arg *current;
+	t_arg 	*current;
+	int		loop;
 
+	loop = 0;
 	current = g_select->args;
 	while (current)
 	{
+		if (current == g_select->args)
+		{
+			if (loop++)
+				break;
+		}
 		if (current->cursor)
 			set_terminal(TEXT_UNDERLINE);
 		if (current->selected)
@@ -142,7 +153,6 @@ void	init_args(int argc, char **argv)
 	t_arg *current;
 	int i;
 
-	g_select = (t_select*)ft_memalloc(sizeof(t_select));
 	current = NULL;
 	i = 1;
 	while (i < argc)
@@ -151,6 +161,7 @@ void	init_args(int argc, char **argv)
 		{
 			current = new_arg(argv[i++], NULL);
 			g_select->args = current;
+			g_select->current = current;
 		}
 		else
 		{
@@ -158,39 +169,119 @@ void	init_args(int argc, char **argv)
 			current = current->next;
 		}
 	}
+	current->next = g_select->current;
+	g_select->current->prev = current;
 }
 
 void	init_termcaps(void)
 {
 	char *terminal_name;
 
+	g_select = (t_select*)ft_memalloc(sizeof(t_select));
 	if (!(isatty(0)))
 		handle_error("Not a terminal");
 	if (!(terminal_name = getenv("TERM")))
 		handle_error("Terminal enviroment variable not found");
-	if (!(tgetent(NULL, terminal_name)))
+	if (tgetent(NULL, terminal_name) < 1)
 		handle_error("Terminal specified in env not found");
-		
+}
+
+int		read_key(void)
+{
+	char c;
+	char sequence[3];
+	
+	c = 0;
+	if (read(0, &c, 1) == -1)
+		handle_error("Read failed.");
+	if (c == 27)
+	{
+		if (read(0, &sequence[0], 1) != 1)
+			return (27);
+		if (read(0, &sequence[1], 1) != 1)
+			return (27);
+		if (sequence[0] == '[')
+			return (sequence[1] - 100);
+	}
+	return c;
+}
+
+int		handle_keys(void)
+{
+	char c;
+
+	c = read_key();
+	if (c == ESCAPE)
+	{
+		ft_printf("\n");
+		restore_terminal_mode();
+		exit(0);
+	}
+	else if (c == ENTER)
+		return (0);
+	else if (c == SPACE)
+	{
+		g_select->current->selected = !g_select->current->selected;
+		c = RIGHT_ARROW;
+	}
+	else if (c == LEFT_ARROW)
+	{
+		g_select->current->cursor = 0;
+		g_select->current = g_select->current->prev;
+		g_select->current->cursor = 1;
+	}
+	if (c == RIGHT_ARROW)
+	{
+		g_select->current->cursor = 0;
+		g_select->current = g_select->current->next;
+		g_select->current->cursor = 1;
+	}
+	return (1);
+}
+
+void	print_selected(void)
+{
+	t_arg 	*current;
+	int		loop;
+
+	loop = 0;
+	current = g_select->args;
+	set_terminal("te");
+	while (current)
+	{
+		if (current == g_select->args)
+		{
+			if (loop++)
+				break;
+		}
+		if (current->selected)
+			ft_fprintf(1, "%s", current->str);
+		current = current->next;
+		if (current)
+			ft_fprintf(0, " ");
+	}
+	ft_printf("\n");
 }
 
 int		main(int argc, char **argv)
 {
 	char c;
 
+	init_termcaps();
+	tcgetattr(0, &g_select->old);
+	if (argc < 2)
+		handle_error("Please include at least one argument");
 	init_signal_handling();
 	init_args(argc, argv);
-	tcgetattr(0, &g_select->old);
-	init_termcaps();
 	set_terminal_raw_mode();
 	while (1)
 	{
 		ft_clear_screen();
 		print_args();
-		if (read(0, &c, 1) == -1)
-			handle_error("Read failed.");
-		if (c == 'q')
+		if (!(handle_keys()))
 			break;
 	}
 	restore_terminal_mode();
+	print_selected();
 	return (0);
 }
