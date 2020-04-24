c8646b849daa7b8203d7428f98eaacf6da67247c
sadawi <hahli144@hotmail.com>
2020-04-24 20:16:08 +0300

    add all prototypes

--- includes/ft_select.h
+++ includes/ft_select.h
@@ -6,7 +6,7 @@
 /*   By: sadawi <sadawi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/10 12:20:24 by sadawi            #+#    #+#             */
-/*   Updated: 2020/04/24 12:25:57 by sadawi           ###   ########.fr       */
+/*   Updated: 2020/04/24 20:16:00 by sadawi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,10 +72,62 @@ typedef struct		s_select
 
 t_select			*g_select;
 
-void	ft_clear_screen(void);
+int					count_rows(t_select *select);
 
-void	print_args(t_select *select);
+int					screen_too_small(t_select *select);
 
-void	handle_signal(int sig);
+void				free_memory(t_select *select);
+
+void				handle_error(t_select *select, char *message, int reset);
+
+int					handle_keys(t_select *select);
+
+int					read_key(t_select *select);
+
+void				handle_control_sequence(t_select *select, char *c);
+
+void				handle_space_key(t_select *select);
+
+void				handle_signal(int sig);
+
+void				handle_signal_suspend(t_select *select);
+
+void				handle_signal_continue(t_select *select);
+
+void				handle_signal_resize(t_select *select);
+
+void				init_termcaps(t_select **select);
+
+void				init_key_sequences(t_select *select);
+
+void				init_signal_handling(t_select *select);
+
+void				init_args(t_select *select, int argc, char **argv);
+
+t_arg				*new_arg(char *str, t_arg *prev);
+
+void				print_args(t_select *select);
+
+void				print_arg(t_select *select, t_arg *arg);
+
+void				print_screen_too_small(t_select *select);
+
+void				print_selected(t_select *select);
+
+void				set_terminal(char *id);
+
+int					ft_putschar(int c);
+
+void				restore_terminal_mode(t_select *select);
+
+void				create_terminal_raw_mode(t_select *select);
+
+void				set_terminal_raw_mode(t_select *select);
+
+void				ft_clear_screen(void);
+
+int					delete_arg(t_select *select, t_arg *arg);
+
+void				select_deselect_all(t_select *select, int boolean);
 
 #endif
